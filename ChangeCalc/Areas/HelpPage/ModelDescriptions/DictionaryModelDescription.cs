b5340c4213a6ce67ab83b36169d7245b0217ba01
Richard Lucas <sherlockdoge@gmail.com>
2017-06-17 12:32:40 +0100

    Add project files.

---
+++ ChangeCalc/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ChangeCalc.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ChangeCalc/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ChangeCalc.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
